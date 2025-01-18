327717757304ac52c25503f61dcbcdb93253aa3d
desireemezzotero <desireemezzotero1@gmail.com>
2025-01-18 12:05:09 +0100

    [add] correct exercise

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,24 @@
-/* import LanguagesFunction from "./Languages" */
-import languages from "../data/languages"
-import { useState } from "react"
+import languages from "../data/languages";
+import { useState } from "react";
 
 const Main = () => {
- const [selectedLanguage, setSelectedLanguage] = useState(false);
-   
- const handleClick = () => {
-  setSelectedLanguage(!selectedLanguage)
- }
-console.log(selectedLanguage)
+  const [selectedLanguage, setSelectedLanguage] = useState(1);
+
   return (
     <main>
       <div className="container text-center">
-        <button type="button" className="btn btn-outline-primary" onClick={handleClick}>html </button>
-        {selectedLanguage && 
-          <div>
-            {languages.description}
-          </div> 
+        {
+          languages.map(language => ( 
+            <button key={language.id} type="button" className="btn btn-outline-primary" onClick={() => setSelectedLanguage(language.id)}>{language.title}</button>
+          ))
         }
+        <div className="container border border-secondary">
+         <h6>{languages.find(language => language.id == selectedLanguage).title}</h6>
+         <p>{languages.find(language => language.id == selectedLanguage).description}</p>
+        </div>
       </div>
     </main>
   )
 }
 
 export default Main
-{/*   <button type="button" className="btn btn-outline-primary" onClick={() => handleClick(2)}>CSS</button>
-  <button type="button" className="btn btn-outline-primary">Javascript</button>
-  <button type="button" className="btn btn-outline-primary">Node.js</button>
-  <button type="button" className="btn btn-outline-primary">Express</button>
-  <button type="button" className="btn btn-outline-primary">ReactJS</button> */}
-  {/*     <LanguagesFunction /> */}
